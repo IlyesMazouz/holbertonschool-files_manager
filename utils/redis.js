001bfc91a32f71e47a97767824dc36476c871a2d
IlyesMazouz <ilyesmazouzz@gmail.com>
2024-07-23 13:09:45 +0200

    connecting to redis

---
+++ utils/redis.js
@@ -0,0 +1,55 @@
+import Redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = Redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error(`Redis client error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(reply ? JSON.parse(reply) : null);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, JSON.stringify(value), 'EX', duration, (err, reply) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(reply);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(reply);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
